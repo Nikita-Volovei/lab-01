03513b7b2b0d48bca49739ab2a23da928d2fde88
Nikita-Volovei <c00306890@setu.ie>
2025-09-15 11:00:08 +0000

    tasks done

---
+++ task2.py
@@ -0,0 +1,3 @@
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter another number:"))
+print("The sum of those 2 numbers is ", (num1+num2))
---
+++ task3.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+StudentID = input("Enter your ID: ")
+print("Your info is;\nAge:", age,"\nName:",name,"\nID:",StudentID)
